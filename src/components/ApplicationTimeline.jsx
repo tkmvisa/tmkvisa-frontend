afbeaf76c062ccc6a29eb4ada1af4749c91e2cfb
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-12-06 01:12:44 +0500

    application status

---
+++ src/components/ApplicationTimeline.jsx
@@ -0,0 +1,146 @@
+"use client"
+import React from 'react';
+import { Timeline, TimelineItem, TimelineSeparator, TimelineDot, TimelineConnector, TimelineContent, TimelineOppositeContent } from '@mui/lab';
+import { Typography, Box } from '@mui/material';
+
+const ApplicationTimeline = () => {
+    return (
+        <section className='mt-4'>
+            <Timeline className='!px-0'>
+                {/* Step 1 */}
+                <TimelineItem>
+                    <TimelineOppositeContent className='hidden' sx={{ flex: 0.3 }}>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                        <TimelineDot color="success" />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <p className='!text-[15px] !font-medium font_man'>
+                            Application has been created
+                        </p>
+                        <p className='!text-[#94A3B8] flex font_man items-center gap-1 !text-[13px] !font-medium'>
+                            <Clock />
+                            09:00, 20 June 2024
+                        </p>
+                    </TimelineContent>
+                </TimelineItem>
+
+                {/* Step 2 */}
+                <TimelineItem>
+                    <TimelineOppositeContent className='hidden' sx={{ flex: 0.3 }}>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <p className='!text-[15px] text-[#94A3B8] !font-semibold font_man'>
+                            Awaiting for invitation
+                        </p>
+                        <p className='!text-[#94A3B8] flex font_man items-center gap-1 !text-[13px] !font-medium'>
+                            <Clock />
+                            16:00, 21 June 2024
+                        </p>
+                    </TimelineContent>
+                </TimelineItem>
+
+                {/* Step 3 */}
+                <TimelineItem>
+                    <TimelineOppositeContent className='hidden' sx={{ flex: 0.3 }}>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <p className='!text-[15px] text-[#94A3B8] !font-semibold font_man'>
+                            Invitation received
+                        </p>
+                        <p className='!text-[#94A3B8] font_man !text-[12px]'>Note:</p>
+                        <p className='!text-[#94A3B8] flex  mt-[2px] items-center gap-1 !text-[13px] !font-medium'>
+                            <Clock />
+                            16:00, 21 June 2024
+                        </p>
+                    </TimelineContent>
+                </TimelineItem>
+
+                {/* Step 4 */}
+                <TimelineItem>
+                    <TimelineOppositeContent className='hidden' sx={{ flex: 0.3 }}>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <p className='!text-[15px] text-[#94A3B8] !font-semibold font_man'>
+                            Awaiting for an appointment
+                        </p>
+                        <p className='!text-[#FE7224] max-w-[270px] font_man !text-[12px]'>Note: Appointment will be given by Consulate. TKMVISA is not responsible for scheduling appointments</p>
+                        <p className='!text-[#94A3B8] flex  mt-[2px] items-center gap-1 !text-[13px] !font-medium'>
+                            <Clock />
+                            16:00, 21 June 2024
+                        </p>
+                    </TimelineContent>
+                </TimelineItem>
+
+                {/* Step 5 */}
+                <TimelineItem>
+                    <TimelineOppositeContent className='hidden' sx={{ flex: 0.3 }}>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                        <TimelineDot />
+                        <TimelineConnector className='invisible'/>
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <p className='!text-[15px] text-[#94A3B8] !font-semibold font_man'>
+                            Appointment has been scheduled
+                        </p>
+                        <p className='!text-[#94A3B8] max-w-[270px] font_man !text-[12px]'>Note: Appointment date is: 17.12.2024</p>
+                        <p className='!text-[#94A3B8] flex  mt-[2px] items-center gap-1 !text-[13px] !font-medium'>
+                            <Clock />
+                            16:00, 21 June 2024
+                        </p>
+                    </TimelineContent>
+                </TimelineItem>
+
+                {/* Step 5 */}
+                <TimelineItem>
+                    <TimelineOppositeContent className='hidden' sx={{ flex: 0.3 }}>
+                    </TimelineOppositeContent>
+                    <TimelineSeparator className='invisible'>
+                        <TimelineDot />
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent>
+                        <p className='!text-[15px] text-[#94A3B8] !font-semibold font_man'>
+                        Completed
+                        </p>
+                        <p className='!text-[#94A3B8] flex  mt-[2px] items-center gap-1 !text-[13px] !font-medium'>
+                            <Clock />
+                            16:00, 21 June 2024
+                        </p>
+                    </TimelineContent>
+                </TimelineItem>
+
+                
+
+                
+
+            </Timeline>
+        </section>
+    );
+};
+
+export default ApplicationTimeline;
+
+
+const Clock = () => {
+    return (
+        <svg width="15" height="14" viewBox="0 0 15 14" fill="none" >
+            <path d="M7.12402 12.25C10.0235 12.25 12.374 9.8995 12.374 7C12.374 4.1005 10.0235 1.75 7.12402 1.75C4.22453 1.75 1.87402 4.1005 1.87402 7C1.87402 9.8995 4.22453 12.25 7.12402 12.25Z" stroke="#94A3B8" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M7.12402 4.08334V7.00001L8.87402 8.75001" stroke="#94A3B8" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    )
+}
