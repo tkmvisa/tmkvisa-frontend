d725403d540a8ddc529727e0ec3cb7ff7a86c3c4
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-12-27 10:16:42 +0500

    update

--- src/components/application-table/application-table.jsx
+++ src/components/application-table/application-table.jsx
@@ -311,7 +311,7 @@ const MuiTableWithSortingAndPagination = ({ applicationsListProps, t }) => {
                                     <TableCell className="!text-xs !font_man capitalize">{row.attributes.Office_Location}</TableCell>
                                     <TableCell className="!text-xs !font_man capitalize">{row.attributes.Who_added}</TableCell>
                                     <TableCell className="!text-xs !font_man" >
-                                        <div className="flex items-center cursor-pointer justify-end" >
+                                        <div className="flex items-center relative cursor-pointer justify-end" >
                                             {/* onClick={()=>handleEdit(row?.id)} */}
                                             <Button
                                                 id="basic-button"
